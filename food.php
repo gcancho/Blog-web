191ab8656e7ad3ec67b9d94256c1d7a03d2fda00
gcancho <giovannicb19@gmail.com>
2020-10-19 23:47:59 -0500

    Vista contacto terminado

--- food.php
+++ food.php
@@ -8,7 +8,114 @@
 </head>
 <body>
     <?php include "header.php" ?>
-
+    <div class="box-gray">
+        <div class="wrap">
+            <h1>Food & Health</h1>
+        </div>
+    </div>
+    <section class="body-page nature">
+        <article class="block-1">
+            <div class="recent-posts">
+                <div class="wrap">
+                    <div class="post">
+                        <img src="./images/food-1.jpg" alt="">
+                        <h3><a href="#">Healthy Food for Healthy Heart</a></h3>
+                        <label class="date-post">TG-Demo Team February 22, 2018 Nature</label>
+                        <p>Duis vitae massa blandit, volutpat enim ac, feugiat lorem. Aenean eget eros a nulla feugiat venenatis. Morbi scelerisque, enim id dapibus mattis, lectus arcu luctus orci, sit amet imperdiet mi arcu et dui. Pellentesque ultrices elementum nunc nec efficitur. Suspendisse</p>
+                        <a class="read-more-2" href="">Read more</a>
+                    </div>
+                    <div class="post">
+                        <img src="./images/food-2.jpg" alt="">
+                        <h3><a href="#">Momos: What Makes This Tibetan Dumpling So Popular?</a></h3>
+                        <label class="date-post">TG-Demo Team February 22, 2018 Nature</label>
+                        <p>Duis vitae massa blandit, volutpat enim ac, feugiat lorem. Aenean eget eros a nulla feugiat venenatis. Morbi scelerisque, enim id dapibus mattis, lectus arcu luctus orci, sit amet imperdiet mi arcu et dui. Pellentesque ultrices elementum nunc nec efficitur. Suspendisse</p>
+                        <a class="read-more-2" href="">Read more</a>
+                    </div>
+                    <div class="post">
+                        <img src="./images/food-3.jpg" alt="">
+                        <h3><a href="#">7 Reasons to Start Your Day with Coffee</a></h3>
+                        <label class="date-post">TG-Demo Team February 22, 2018 Nature</label>
+                        <p>Duis vitae massa blandit, volutpat enim ac, feugiat lorem. Aenean eget eros a nulla feugiat venenatis. Morbi scelerisque, enim id dapibus mattis, lectus arcu luctus orci, sit amet imperdiet mi arcu et dui. Pellentesque ultrices elementum nunc nec efficitur. Suspendisse</p>
+                        <a class="read-more-2" href="">Read more</a>
+                    </div>
+                </div>
+            </div>
+        </article>
+        <article class="block-2">
+            <div class="about-me">
+                <h2 class="subtitle">About Me</h2>
+                <div class="wrap">
+                    <img src="./images/about-me.jpg" alt="">
+                    <label>I am an author who loves the nature and travel, travel and food.</label>
+                    <div class="social-networks">
+                        <a href="#"><i class="fa fa-facebook"></i></a>
+                        <a href="#"><i class="fa fa-twitter"></i></a>
+                        <a href="#"><i class="fa fa-google-plus"></i></a>
+                        <a href="#"><i class="fa fa-instagram"></i></a>
+                        <a href="#"><i class="fa fa-youtube"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="gallery">
+                <h2 class="subtitle">Gallery</h2>
+                <div class="wrap">
+                    <div class="images-gallery">
+                        <div class="columns-3">
+                            <img src="./images/gallery-1.jpg" alt="">
+                            <img src="./images/gallery-2.jpg" alt="">
+                            <img src="./images/gallery-3.jpg" alt="">
+                        </div>
+                        <div class="columns-3">
+                            <img src="./images/gallery-4.jpg" alt="">
+                            <img src="./images/gallery-5.jpg" alt="">
+                            <img src="./images/gallery-6.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="popular">
+                <h2 class="subtitle">Recent Posts</h2>
+                <div class="wrap">
+                    <div class="item-popular">
+                        <div class="container-img">
+                            <img src="./images/popular-1.jpg" alt="">
+                        </div>
+                        <div class="container-texto-popular">
+                            <h3>Architectural Trends That Will Define The Next Decade</h3>
+                            <label>TG-Demo Team February 22, 2018 Nature</label>
+                        </div>
+                    </div>
+                    <div class="item-popular">
+                        <div class="container-img">
+                            <img src="./images/popular-2.jpg" alt="">
+                        </div>
+                        <div class="container-texto-popular">
+                            <h3>Best Place to Visit This Spring</h3>
+                            <label>TG-Demo Team February 21, 2018 Nature</label>
+                        </div>
+                    </div>
+                    <div class="item-popular">
+                        <div class="container-img">
+                            <img src="./images/popular-3.jpg" alt="">
+                        </div>
+                        <div class="container-texto-popular">
+                            <h3>Trekking to the Himalayas</h3>
+                            <label>TG-Demo Team February 21, 2018 Nature</label>
+                        </div>
+                    </div>
+                    <div class="item-popular">
+                        <div class="container-img">
+                            <img src="./images/popular-4.jpg" alt="">
+                        </div>
+                        <div class="container-texto-popular">
+                            <h3>Life Under the Sea</h3>
+                            <label>TG-Demo Team February 21, 2018 Nature</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </article>
+    </section>
     <?php include "footer.php" ?>
     <?php include "scripts.php" ?>  
 </body>
